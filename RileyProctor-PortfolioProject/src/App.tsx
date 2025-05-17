eb58979d71177a06653321a471c308f5b841cf91
Riley Proctor <riley.proctor2006@gmail.com>
2025-05-17 07:29:09 +0100

    Improved Contact Section & Caret Fix
    Improved contact section with socials icons and links, mail me section, footer at the bottom.
    Fixed carets showing up on text and shortened intro border

--- RileyProctor-PortfolioProject/src/App.tsx
+++ RileyProctor-PortfolioProject/src/App.tsx
@@ -1,5 +1,11 @@
+
+//Site Components
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Projects from './components/Projects'
+import About from './components/About'
+import Contact from './components/Contact'
+
 import React from 'react';
 
 function App() {
@@ -8,21 +14,9 @@ function App() {
             <Navbar />
             <main className="">
                 <Hero />
-                <section id="projects" className="min-h-screen max-w-6xl mx-auto px-6 py-20">
-                    <h2 className="text-4xl font-bold mb-8">Projects</h2>
-                    {/* TODO: Add my projects grid here */}
-                    <p className="text-gray-600">Coming Soon...</p>
-                </section>
-                <section id="about" className="min-h-screen max-w-4xl mx-auto px-6 py-20">
-                    <h2 className="text-4xl font-bold mb-8">About Me</h2>
-                    {/* TODO: Add my about content here */}
-                    <p className="text-gray-700">I'm a passionate game developer with 6 years of programming experience...</p>
-                </section>
-                <section id="contact" className="bg-gray-900 text-white py-24 min-h-[90vh] text-center">
-                    <h2 className="text-3xl font-bold mb-6">Contact Me Here</h2>
-                    {/* TODO: Add my contacts/social links here */}
-                    <p>Email me at <a href="mailto:rileypgames06@gmail.com" className="underline hover:text-blue-400">rileypgames06@gmail.com</a></p>
-                </section>
+                <Projects />
+                <About />
+                <Contact />
             </main>
         </div>
     );
---
+++ RileyProctor-PortfolioProject/src/components/About.tsx
@@ -0,0 +1,13 @@
+﻿import React from 'react'
+
+const About = () => {
+    return (
+        <section id="about" className="min-h-screen max-w-4xl mx-auto px-6 py-20">
+            <h2 className="text-4xl font-bold mb-8">About Me</h2>
+            {/* TODO: Add my about content here */}
+            <p className="text-gray-700">I'm a passionate game developer with 6 years of programming experience...</p>
+        </section>
+    );
+};
+
+export default About;
