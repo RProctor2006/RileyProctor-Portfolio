36ee75f37fd1422b7f596b150ba7d3c16b593dea
Riley <riley.proctor2006@gmail.com>
2025-07-16 00:24:08 +0100

    Mobile Issues Fixed
    Fixed problem where:
    burger menu was appearing on laptop and larger screen sizes
    
    mobile sized text appearing on laptop and larger screen sizes
    
    Fixed formatting on header list

--- RileyProctor-PortfolioProject/src/components/BurgerMenu.tsx
+++ RileyProctor-PortfolioProject/src/components/BurgerMenu.tsx
@@ -1,11 +1,8 @@
 import { FaBars } from "react-icons/fa";
 import { useState } from 'react';
+import { Link } from 'react-scroll'
 
-type MenuProps = {
-    list: React.ReactNode;
-}
-
-const BurgerMenu = ({list}:MenuProps) => {
+const BurgerMenu = () => {
 
     const [isPressed, setIsPressed] = useState(false);
     
@@ -18,8 +15,26 @@ const BurgerMenu = ({list}:MenuProps) => {
             </button>
 
             {isPressed && (
-                <div className="h-fit py-1 px-3 absolute mt-2 mx-auto bg-white dark:bg-gray-900 shadow-lg z-50 text-black dark:text-white">
-                    {list}
+                <div
+                    className="h-fit py-1 px-3 absolute mt-2 mx-auto bg-white dark:bg-gray-900 shadow-lg z-50 text-black dark:text-white">
+                    <ul className="sm:text-sm flex flex-col gap-8 text-gray-700 font-medium md:text-xl dark:text-gray-300 dark:shadow-white">
+                        <li><Link to="home" smooth={true} duration={500} spy={true} offset={-80}
+                                  activeClass="font-semibold"
+                                  className="hover:text-darklightsec cursor-pointer transition-colors duration-300">Home</Link>
+                        </li>
+                        <li><Link to="projects" smooth={true} duration={500} spy={true} offset={-80}
+                                  activeClass="font-semibold"
+                                  className="hover:text-darklightsec cursor-pointer transition-colors duration-300">Projects</Link>
+                        </li>
+                        <li><Link to="about" smooth={true} duration={500} spy={true} offset={-80}
+                                  activeClass="font-semibold"
+                                  className="hover:text-darklightsec cursor-pointer transition-colors duration-300">About</Link>
+                        </li>
+                        <li><Link to="contact" smooth={true} duration={500} spy={true} offset={-80}
+                                  activeClass="font-semibold"
+                                  className="hover:text-blue-600 cursor-pointer transition-colors duration-300">Contact</Link>
+                        </li>
+                    </ul>
                 </div>
             )}
         </div>
--- RileyProctor-PortfolioProject/src/components/Navbar.tsx
+++ RileyProctor-PortfolioProject/src/components/Navbar.tsx
@@ -13,42 +13,36 @@ const Navbar = () => {
     useEffect(() => {
         scrollSpy.update();
     }, []);
-    
-    const medBreakpoint = 768;
-    
-    const navbarHeadings = (
-        <ul className="sm:text-sm flex flex-col gap-8 text-gray-700 font-medium text-xl dark:text-gray-300 dark:shadow-white">
-            <li><Link to="home" smooth={true} duration={500} spy={true} offset={-80}
-                      activeClass="font-semibold"
-                      className="hover:text-darklightsec cursor-pointer transition-colors duration-300">Home</Link>
-            </li>
-            <li><Link to="projects" smooth={true} duration={500} spy={true} offset={-80}
-                      activeClass="font-semibold"
-                      className="hover:text-darklightsec cursor-pointer transition-colors duration-300">Projects</Link>
-            </li>
-            <li><Link to="about" smooth={true} duration={500} spy={true} offset={-80}
-                  activeClass="font-semibold"
-                  className="hover:text-darklightsec cursor-pointer transition-colors duration-300">About</Link>
-            </li>
-            <li><Link to="contact" smooth={true} duration={500} spy={true} offset={-80}
-                  activeClass="font-semibold"
-                  className="hover:text-blue-600 cursor-pointer transition-colors duration-300">Contact</Link>
-            </li>
-        </ul>
-    )
 
     return (
         <nav
-            className="fixed  sm:w-dvw sm:text-sm top-0 left-0 right-0 bg-lightacc1/80 backdrop-blur-md shadow-md z-50 text-lighttext dark:bg-darklight/80 dark:text-white dark:shadow-xl/30">
+            className="fixed sm:w-dvw sm:text-sm top-0 left-0 right-0 bg-lightacc1/80 backdrop-blur-md shadow-md z-50 text-lighttext dark:bg-darklight/80 dark:text-white dark:shadow-xl/30">
             <div className="max-w-6xl mx-auto  py-4 flex justify-between items-center">
-                <div className="sm:text-sm text-2xl font-bold">Riley Proctor │ Gameplay Programmer</div>
+                <div className="sm:text-sm md:text-2xl font-bold">Riley Proctor │ Gameplay Programmer</div>
                 <ThemeSwitchButton/>
 
                 {/*If device viewport is larger than phone size, then display headings in navbar*/}
-                {window.innerWidth < medBreakpoint ? (
-                    navbarHeadings
+                {window.innerWidth > 768 ? (
+                    <ul className="sm:text-sm flex flex-row gap-8 text-gray-700 font-medium md:text-xl dark:text-gray-300 dark:shadow-white">
+                        <li><Link to="home" smooth={true} duration={500} spy={true} offset={-80}
+                                  activeClass="font-semibold"
+                                  className="hover:text-darklightsec cursor-pointer transition-colors duration-300">Home</Link>
+                        </li>
+                        <li><Link to="projects" smooth={true} duration={500} spy={true} offset={-80}
+                                  activeClass="font-semibold"
+                                  className="hover:text-darklightsec cursor-pointer transition-colors duration-300">Projects</Link>
+                        </li>
+                        <li><Link to="about" smooth={true} duration={500} spy={true} offset={-80}
+                                  activeClass="font-semibold"
+                                  className="hover:text-darklightsec cursor-pointer transition-colors duration-300">About</Link>
+                        </li>
+                        <li><Link to="contact" smooth={true} duration={500} spy={true} offset={-80}
+                                  activeClass="font-semibold"
+                                  className="hover:text-blue-600 cursor-pointer transition-colors duration-300">Contact</Link>
+                        </li>
+                    </ul>
                 ) : (
-                    <BurgerMenu list={navbarHeadings} />
+                    <BurgerMenu />
                 )}
 
             </div>
