3aef54869ce250ce814fd1813c3d74872f83658c
Riley Proctor <riley.proctor2006@gmail.com>
2025-09-02 14:04:18 +0100

    Finished Combat Section
    Finished talking about the combat system for TFH.

--- RileyProctor-PortfolioProject/src/pages/TheFirstHeroPage.tsx
+++ RileyProctor-PortfolioProject/src/pages/TheFirstHeroPage.tsx
@@ -47,7 +47,7 @@ function TheFirstHeroPage() {
                     <h1 className="text-3xl ">Combat System</h1>
 
                     {/*Animation Montages Section*/}
-                    <div className="flex flex-row pt-6">
+                    <div className="flex flex-row pt-4">
                         <img className="h-85" src="/images/placeholders/TFH_AnimNotifs_Placeholder.png" alt="TFH_AnimNotifs Placeholder" />
                         <div className="dark:bg-darkbgsec ml-3 shadow-lg shadow-lightacc2 outline-solid outline-lightacc2 rounded-xl">
                             <div className="p-4 text-wrap text-left">
@@ -56,6 +56,23 @@ function TheFirstHeroPage() {
                                 in Unreal Engine. I utilised the animation notify classes in order to trigger certain events. This includes: Activating weapon hitboxes,
                                 Motion Warping, Invincibility Frames on rolls, Input windows and more!</p>
                                 <p className="text-lg pt-4">This system is ideal as it allows for more natural feeling combat, instead of inconveniencing myself with hard coded timings, I can make dynamic, reusable notify events that I can add to whatever animation montage I require.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Lock On Section*/}
+                    <div className="flex flex-row pt-10">
+                        <img className="h-85" src="/images/projects/TFH/TFH_LockOnFlowchart.png" alt="TFH_AnimNotifs Placeholder" />
+                        <div className="dark:bg-darkbgsec ml-3 shadow-lg shadow-lightacc2 outline-solid outline-lightacc2 rounded-xl">
+                            <div className="p-4 text-wrap text-left">
+                                <h1 className="text-3xl">Lock On System</h1>
+                                <p className="text-lg pt-4">The lock on system is one of the most complex systems in the project. I revisited it often during development
+                                in order to account for new situations. As seen in the flowchart, the way it works is that I fire a sphere trace that detects all the enemies
+                                in the area. These enemies are then stored in array, after that I create a sphere collision which is attached to the player character, this
+                                will detect any new enemies that enter lock on range, it will check if they are already in the array, if not, they will be added into it. It 
+                                will then ascertain the closest enemy and lock onto them.</p>
+                                <p className="text-lg pt-4">To switch targets, it just finds the next closest enemy in the array and changes target.</p>
+                                
                             </div>
                         </div>
                     </div>
@@ -72,6 +89,17 @@ function TheFirstHeroPage() {
                                 other components, namely, the health and equipment components I have, which I will explain in further detail later. </p>
                             </div>
                         </div>
+                    </div>
+
+                    {/*Combat System Showcase*/}
+                    <div className="pt-6 flex-col justify-items-start">
+                        <h1 className="text-3xl">Showcase</h1>
+                        <iframe width="960" height="540"
+                                src="https://www.youtube.com/embed/K8uVbyoCZ8s?si=xHEGanYorwEvBw2u"
+                                title="Combat Showcase Video" frameBorder="0"
+                                allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerPolicy="strict-origin-when-cross-origin"
+                                allowFullScreen></iframe>
                     </div>
                 </div>
             </div>
