94b6046ece586f7ac2d0e07d9854bbf335811db5
Riley Proctor <riley.proctor2006@gmail.com>
2025-12-04 13:08:14 +0000

    Scrolls from Navbar

--- RileyProctor-PortfolioProject/src/App.tsx
+++ RileyProctor-PortfolioProject/src/App.tsx
@@ -8,10 +8,27 @@ import HomePage from './pages/HomePage.tsx'
 import TheFirstHeroPage from "./pages/TheFirstHeroPage.tsx";
 
 //import essentials
-import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { scroller } from 'react-scroll';
 
 
 function App() {
+    
+    //Get location as variable
+    const location = useLocation();
+    
+    //If the location has a hashtag in it, remove it and scroll to that location
+    useEffect(() => {
+        if (location.hash) {
+            const id = location.hash.replace("#", "");
+            scroller.scrollTo(id, {
+                smooth: true,
+                duration: 500,
+                offset: -80,
+            });
+        }
+    }, [location]);
     
     return (
         <div id="root" className="w-full h-full mx-auto p-8 text-center bg-white text-black dark:bg-darkbg dark:text-white">
