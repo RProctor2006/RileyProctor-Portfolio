cde943fd155d877c2e06ae18075bbc474f83ae0c
Riley Proctor <riley.proctor2006@gmail.com>
2025-12-06 21:47:43 +0000

    Enlarged Showcase Vid on sm
    Enlarged the initial showcase video on smaller screens (mobile)

--- RileyProctor-PortfolioProject/src/components/Hero.tsx
+++ RileyProctor-PortfolioProject/src/components/Hero.tsx
@@ -2,14 +2,16 @@
 
 const Hero = () => {
     return (
-        <section id="home" className="mx-auto sm:mt-28 md:h-auto md:w-full lg:w-1/2 lg:h-auto md:mt-15 lg:mt-12 ">
-            <video
-                className=" md:min-w-full"
-                src="/videos/theFirstHero/CombatShowcase.mp4"
-                autoPlay={true}
-                loop={true}
-                muted={true}
-            ></video>
+        <section id="home" className="mx-auto sm:mt-18 md:h-auto md:w-full lg:w-1/2 lg:h-auto md:mt-15 lg:mt-12 ">
+            <div className="w-full flex justify-center">
+                <video
+                    className="sm:min-w-[45vh] md:min-w-full"
+                    src="/videos/theFirstHero/CombatShowcase.mp4"
+                    autoPlay={true}
+                    loop={true}
+                    muted={true}
+                ></video>
+            </div>
         </section>
     );
 };
